c245c0e3195a4e4d7cad0c597992c144b4899887
FaIzAnsaRi715 <167748258+FaIzAnsaRi715@users.noreply.github.com>
2025-07-24 18:18:37 +0530

    Update openAI.py

--- openAI.py
+++ openAI.py
@@ -2,15 +2,11 @@ from openai import OpenAI
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key="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"
+    api_key="Enter your API key"
 )
 
 command = '''
-[09:53, 7/24/2025] GadHuu❤️: Good morning babe 😘❤️
-[12:07, 7/24/2025] FaIz: Good morning 😒
-[12:07, 7/24/2025] GadHuu❤️: Sorry babe kal pata nhi kese nind lakgayi
-[13:12, 7/24/2025] FaIz: Acha
-😒
+
 '''
 
 completion = client.chat.completions.create(
